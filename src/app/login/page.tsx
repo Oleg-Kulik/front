e5c02bd8d5ee6ce9733881be62a5243eaf519af3
Oleg Kulik <kulik.ol.val@gmail.com>
2025-03-23 18:21:41 +0700

    работал над страницей входа, немного переделал структуру папок, подключил UI Shadcn, прикрутил валидацию через Zod, добавил элементы: Form, Input, Button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,73 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    email: z.string().email({
+        message: "Введите адрес электронной почты",
+    }),
+    password: z.string().min(1, {
+        message: "Введите пароль",
+    }),
+})
+
 export default function Blank() {
-    return (<div>blank</div>);
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        resolver: zodResolver(formSchema),
+    });
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        console.log(data)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-lg">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Электронная почта</FormLabel>
+                            <FormControl>
+                                <Input placeholder="example@mail.ru" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Пароль</FormLabel>
+                            <FormControl>
+                                <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Войти</Button>
+            </form>
+        </Form>
+
+    );
 }
